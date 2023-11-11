8c628f1c0824944636bcb9f11777dcc15c7cc1ef
rake5h1 <rakeshkapgate1@gmail.com>
2023-11-11 11:13:27 +0530

    'changes'

--- features_files/login.feature
+++ features_files/login.feature
@@ -1,6 +1,7 @@
-Feature:Login Successfully
+Feature: Login Successfully
 
-   Scenario: Test Login
-    Given Visit Site
-    When I Enter Username and Password
-   #  Then Click Login
+Scenario: Test Login
+  Given a browser
+  Given Visit Site
+  When I Enter Username and Password
+  # Then Click Login
--- steps/step_definitions_login.py
+++ steps/step_definitions_login.py
@@ -1,29 +1,39 @@
 from behave import given, when, then
 from selenium import webdriver
-from pages.login import Login
-from selenium.webdriver.chrome.options import Options
-# import os
+from selenium.webdriver.edge.options import Options
+from Pages.login import Login
+
 options = Options()
-options.add_argument('--headless')
+options.add_argument("--headless")
+
+
+def browser():
+    driver = webdriver.Edge(options=options)
+    return driver
+
+
+@given("a browser")
+def get_browser(context):
+    context.browser = browser()
+
 
 @given("Visit Site")
 def visit_site(context):
     # edge_driver_path = os.environ.get("EDGE_DRIVER_PATH")
-    context.driver = webdriver.Edge(options=options)
-    context.login_test = Login(context.driver)
-    context.driver.get(
+    context.login_test = context.browser
+    context.browser.get(
         "https://opensource-demo.orangehrmlive.com/web/index.php/auth/login"
     )
-    context.driver.maximize_window()
+    context.browser.maximize_window()
 
 
 @when("I Enter Username and Password")
 def enter_details(context):
-    context.login_test.enter_details()
+    Login(context.login_test).enter_details()
 
 
 @then("Click Login")
 def click_login(context):
     # context.login_test.click_login()
 
-    context.driver.quit()
+    context.browser.quit()

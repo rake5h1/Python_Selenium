87709bef438e30e771ab67d4af24753c21d157ee
rake5h1 <rakeshkapgate1@gmail.com>
2023-11-11 10:08:54 +0530

    'changes'

--- steps/step_definitions_login.py
+++ steps/step_definitions_login.py
@@ -1,13 +1,13 @@
 from behave import given, when, then
 from selenium import webdriver
 from pages.login import Login
-from selenium.webdriver.edge.service import Service
-import os
+# from selenium.webdriver.edge.service import Service
+# import os
 
 @given("Visit Site")
 def visit_site(context):
-    edge_driver_path = os.environ.get("EDGE_DRIVER_PATH")
-    context.driver = webdriver.Edge(edge_driver_path)
+    # edge_driver_path = os.environ.get("EDGE_DRIVER_PATH")
+    context.driver = webdriver.Edge()
     context.login_test = Login(context.driver)
     context.driver.get(
         "https://opensource-demo.orangehrmlive.com/web/index.php/auth/login"

b3f6e2f9de0a0c287eb475a351320630268b0d52
rake5h1 <rakeshkapgate1@gmail.com>
2023-11-11 09:47:07 +0530

    'changes'

--- steps/step_definitions_login.py
+++ steps/step_definitions_login.py
@@ -1,19 +1,27 @@
 from behave import given, when, then
 from selenium import webdriver
 from pages.login import Login
+from selenium.webdriver.edge.service import Service
 
-@given('Visit Site')
+
+@given("Visit Site")
 def visit_site(context):
-    context.driver=webdriver.Edge()
-    context.login_test=Login(context.driver)
-    context.driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login")
+    edge_driver_path = "msedgedriver.exe"
+    edge_service = Service(edge_driver_path)
+    context.driver = webdriver.Edge(service=edge_service)
+    context.login_test = Login(context.driver)
+    context.driver.get(
+        "https://opensource-demo.orangehrmlive.com/web/index.php/auth/login"
+    )
     context.driver.maximize_window()
 
-@when('I Enter Username and Password')
+
+@when("I Enter Username and Password")
 def enter_details(context):
     context.login_test.enter_details()
 
-@then('Click Login')
+
+@then("Click Login")
 def click_login(context):
     # context.login_test.click_login()
 
